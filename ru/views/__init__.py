503d64b6a31ee995db381e75e1741e41e0174a81
dj-lei <m18349125880@gmail.com>
2020-11-18 16:58:51 +0800

    Add files

--- ru/views/__init__.py
+++ ru/views/__init__.py
@@ -12,19 +12,5 @@ from elasticsearch import Elasticsearch
 cf = configparser.ConfigParser()
 cf.read('ru/config/ru.cfg')
 
-# ENV_PROFILE = os.getenv("ENV")
-#
-# if ENV_PROFILE == "production":
-#     DATABASES = {                 #生产环境数据库配置
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': '数据库名',
-#             'USER': 'root',
-#             'PASSWORD': '密码',
-#             'HOST': '生产环境数据库地址',
-#             'PORT': '3306',
-#         }
-#     }
-# else:
-
 es_ctrl = Elasticsearch([{'host': '10.166.152.49', 'port': 9200}])
+# es_ctrl = Elasticsearch([{'host': 'localhost', 'port': 9200}])
