06ec7c53d608414aa602077c6a111965cfc1a2be
dj-lei <m18349125880@gmail.com>
2020-12-01 15:49:41 +0800

    Add files

--- ru/utils/common.py
+++ ru/utils/common.py
@@ -1,3 +1,9 @@
+import gzip
+import base64
+from PIL import Image
+from io import BytesIO
+
+
 def query_dict(key, value):
     doc = {
         "query": {
@@ -7,3 +13,33 @@ def query_dict(key, value):
         }
     }
     return doc
+
+
+def image_to_base64(image_path):
+    pic = Image.open(image_path)
+    pic = pic.convert('RGB')
+    w, h = pic.size
+    if w > 800:
+        pic = pic.resize((800, int(h / (w / 800))), Image.ANTIALIAS)
+    else:
+        pic = pic.resize((w, h), Image.ANTIALIAS)
+    output_buffer = BytesIO()
+    pic.save(output_buffer, format='JPEG', quality=50)
+    byte_data = output_buffer.getvalue()
+    base64_str = base64.b64encode(byte_data)
+    return base64_str.decode()
+
+
+def base64_to_image(base64_str):
+    byte_data = base64.b64decode(base64_str)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    return img
+
+
+def file_compress(file):
+    return gzip.compress(file)
+
+
+def file_decompress(file):
+    return gzip.decompress(eval(file)).decode()
--- ru/views/trouble_shooting_views.py
+++ ru/views/trouble_shooting_views.py
@@ -1,5 +1,7 @@
 from ru.views import *
 from ru.utils.mind_handle import *
+from io import BytesIO
+from PIL import Image
 
 
 def get(request):
@@ -22,14 +24,6 @@ def get(request):
                 template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
                 _ = es_ctrl.delete(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TEMPLATE'], id=template_id)
                 return JsonResponse({'content': 'Success'})
-            elif operate == cf['TROUBLE_SHOOTING']['RELEASE_TASK']:
-                template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
-                res = es_ctrl.get(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TEMPLATE'], id=template_id)['_source']
-                res = json.loads(re.sub(r'\"topic\"', '"Status": "active", "Executor": "pending", "style": {"fontWeight": "bold", "color": "#f1c40e"}, "topic"', json.dumps(res)))
-                res['Status'] = cf['TROUBLE_SHOOTING']['STATUS_ACTIVE']
-                res['Date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                _ = es_ctrl.index(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'], body=res)
-                return JsonResponse({'content': 'Success'})
             elif operate == cf['TROUBLE_SHOOTING']['GET_TASK_TITLES']:
                 res = es_ctrl.search(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'])
                 data = res['hits']['hits']
@@ -45,6 +39,15 @@ def get(request):
             elif operate == cf['TROUBLE_SHOOTING']['DELETE_TASK']:
                 template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
                 _ = es_ctrl.delete(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'], id=template_id)
+
+                _id = es_ctrl.search(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_IMAGES'],
+                                     body=query_dict('task_id', template_id))['hits']['hits'][0]['_id']
+                _ = es_ctrl.delete(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_IMAGES'], id=_id)
+
+                _id = es_ctrl.search(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_LOGS'],
+                                     body=query_dict('task_id', template_id))['hits']['hits'][0]['_id']
+                _ = es_ctrl.delete(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_LOGS'], id=_id)
+
                 return JsonResponse({'content': 'Success'})
             elif operate == cf['TROUBLE_SHOOTING']['CLOSE_TASK']:
                 template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
@@ -67,6 +70,20 @@ def get(request):
                 for elm in data:
                     result.append({'id': elm[0], 'Task': elm[1], 'Status': elm[2], 'Executor': elm[3]})
                 return JsonResponse({'content': result, 'id': template_id})
+            elif operate == cf['TROUBLE_SHOOTING']['GET_TASK_IMAGES']:
+                template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
+                res = es_ctrl.search(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_IMAGES'], body=query_dict('task_id', template_id))['hits']['hits'][0]['_source']['content']
+                result = []
+                for elm in res:
+                    result.append("data:image/png;base64," + elm['content'])
+                return JsonResponse({'content': result})
+            elif operate == cf['TROUBLE_SHOOTING']['GET_TASK_LOGS']:
+                template_id = request.GET.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
+                res = es_ctrl.search(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_LOGS'], body=query_dict('task_id', template_id))['hits']['hits'][0]['_source']
+                response = HttpResponse(file_decompress(res['content']), 'application/txt')
+                response['Content-Disposition'] = res['name']
+                response['Access-Control-Expose-Headers'] = 'Content-Disposition'
+                return response
         return HttpResponse(404)
     except Exception as e:
         traceback.print_exc()
@@ -124,6 +141,33 @@ def save(request):
                 _ = es_ctrl.delete(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'], id=template_id)
                 _ = es_ctrl.index(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'], body=result, id=template_id)
                 return JsonResponse({'content': result})
+            elif operate == cf['TROUBLE_SHOOTING']['RELEASE_TASK']:
+                template_id = request.POST.get(cf['TROUBLE_SHOOTING']['TEMPLATE_ID'])
+                description = request.POST.get(cf['TROUBLE_SHOOTING']['GET_DESCRIPTION'])
+                logs = request.FILES.get(cf['TROUBLE_SHOOTING']['GET_LOGS'])
+                size = request.POST.get(cf['TROUBLE_SHOOTING']['GET_SIZE'])
+
+                res = es_ctrl.get(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TEMPLATE'], id=template_id)['_source']
+                res = json.loads(re.sub(r'\"topic\"', '"Status": "active", "Executor": "pending", "style": {"fontWeight": "bold", "color": "#f1c40e"}, "topic"', json.dumps(res)))
+                res['Description'] = description
+                res['Status'] = cf['TROUBLE_SHOOTING']['STATUS_ACTIVE']
+                res['Date'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                res = es_ctrl.index(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK'], body=res)
+
+                if int(size) > 0:
+                    base64_images = []
+                    for i in range(0, int(size)):
+                        image = request.FILES.get(cf['TROUBLE_SHOOTING']['GET_IMAGES']+'_'+str(i))
+                        base64_images.append({'name': str(i) + '.jpg', 'content': image_to_base64(image)})
+                    data = {'task_id': res['_id'], 'content': base64_images}
+                    _ = es_ctrl.index(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_IMAGES'], body=data)
+
+                if logs is not None:
+                    cp = file_compress(logs.read())
+                    data = {'task_id': res['_id'], 'name': logs.name, 'content': str(cp)}
+                    _ = es_ctrl.index(index=cf['TROUBLE_SHOOTING']['ES_INDEX_TASK_LOGS'], body=data)
+
+                return JsonResponse({'content': 'Success'})
         return HttpResponse(404)
     except Exception as e:
         traceback.print_exc()
