ffa8b45033c9aab55d0f9bf1c7b37798227bbbb5
dj-lei <m18349125880@gmail.com>
2020-12-09 10:47:45 +0800

    Add cors

--- be/settings.py
+++ be/settings.py
@@ -58,6 +58,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    'http://x-brain.cdlab.cn.ao.ericsson.se',
 )
 
 CORS_ALLOW_METHODS = (
